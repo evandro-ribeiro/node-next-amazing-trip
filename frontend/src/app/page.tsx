7f351b2867d5c4561828da13d54135130540b8e8
evandro-ribeiro <evandrormf.dev@gmail.com>
2024-12-24 15:41:45 -0300

    Add titulo na home

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -4,6 +4,7 @@ import Map from "./components/Map";
 export default function Home() {
   return (
     <div>
+      <h1 className="title">Amazing Trips</h1>
       <Map />
       <Drivers />
     </div>
